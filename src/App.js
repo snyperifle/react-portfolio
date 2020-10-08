e8ae3b4e194b639101fbad62b12d0c0079c41d18
mugen 430 <acampado.maverick@gmail.com>
2020-10-08 00:17:16 -0500

    added routes and other pages in the navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="demo-big-content">
+      <Layout>
+        <Header title="Maverick Acampado" scroll>
+          <Navigation>
+            <Link to="/main">Home</Link>
+            <Link to="/resume">Resume</Link>
+            <Link to="/projects">Portfolio</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/about">About Me</Link>
+          </Navigation>
+        </Header>
+        <Drawer title="Maverick Acampado">
+          <Navigation>
+            <Link to="/main">Home</Link>
+            <Link to="/resume">Resume</Link>
+            <Link to="/projects">Portfolio</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/about">About Me</Link>
+          </Navigation>
+        </Drawer>
+        <Content>
+          <div className="page-content" />
+          <Main />
+        </Content>
+      </Layout>
     </div>
   );
 }
---
+++ src/components/main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import LandingPage from './landing';
+import AboutMe from './about';
+import Contact from './contact';
+import Projects from './projects';
+import Resume from './resume';
+
+
+
+const Main = () => {
+    return (
+        <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/about" component={AboutMe} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/resume" component={Resume} />
+        </Switch>
+    );
+}
+
+export default Main;
