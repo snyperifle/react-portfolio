36224ea12764124a4f2663cae69fd8eb1839138d
Calvin <calvinchui382025@yahoo.com>
2020-10-11 06:09:27 -0500

    testing new static file serving

--- server/index.js
+++ server/index.js
@@ -3,11 +3,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', (req, res) => {
-   res.sendFile(path.join(__dirname, 'build', 'index.html'));
- });
+// app.get('/*', (req, res) => {
+//    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//  });
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', function(req, res) {
+   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 app.set('port', port);
 
