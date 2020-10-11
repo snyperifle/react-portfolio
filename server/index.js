5cb380473b885795517b5c31c8d1ceeb8e82ea3d
Calvin <calvinchui382025@yahoo.com>
2020-10-11 05:53:48 -0500

    updated static serve files

--- server/index.js
+++ server/index.js
@@ -1,14 +1,13 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3001;
 
-app.use(express.static(publicPath));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
-});
+app.get('/*', (req, res) => {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ });
 
 app.set('port', port);
 
